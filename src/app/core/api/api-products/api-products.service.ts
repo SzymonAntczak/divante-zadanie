7400713280be69583c1a19e1f2631e072b886815
Szymon Anczak <szymon0antczak@gmail.com>
2019-05-06 00:21:51 +0200

    Api

--- src/app/core/api/api-products/api-products.service.ts
+++ src/app/core/api/api-products/api-products.service.ts
@@ -11,11 +11,11 @@ export class ApiProductsService {
   constructor(private http: HttpClient) { }
 
   public getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${api}/products`);
+    return this.http.get(`${api}/products`);
   }
 
   public getProductById(productId: string): Observable<Product[]> {
-    return this.http.get<Product[]>(`${api}/products`).pipe(
+    return this.http.get(`${api}/products`).pipe(
       map(products => products.filter(product => product.id === productId))
     );
   }
